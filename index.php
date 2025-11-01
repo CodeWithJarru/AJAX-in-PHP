9574812802fbc7a81d1ae6d7c55875628b72079d
CodeWithJarru <164654447+CodeWithJarru@users.noreply.github.com>
2025-11-01 17:13:02 +0500

    title changed

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $db = mysqli_connect('localhost', 'root', '', 'ajax');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AJAX ko PHP me use krna</title>
+    <title>Using AJAX in PHP</title>
     <style>
         * {
             margin: 0;
